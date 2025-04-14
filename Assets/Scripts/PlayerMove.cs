9354a60a39c4c2d865619e966f4f9e086eef8f59
mayaheilbrun1 <maya.heilbrun@mail.huji.ac.il>
2025-04-14 18:15:27 +0300

    try to fix jump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,61 +3,75 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-        public Rigidbody2D rb;
-        public Transform groundCheck;
-        public LayerMask groundLayer;
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
-        private float horizontal;
-        private float speed = 8f;
-        private float jumpingPower = 16f;
-        private bool isFacingRight = true;
+    private float horizontal;
+    private float speed = 8f;
+    private bool isFacingRight = true;
 
-        void Update()
+    [Header("Jump Tweaks")]
+    [Range(0f, 18f)]
+    public float jumpingPower = 8f; 
+    [Range(0f, 8f)]public  float fallMultiplier = 2.5f;
+    [Range(0f, 8f)]public float lowJumpMultiplier = 2f;
+
+    private bool jumpHeld = false;
+
+    void Update()
+    {
+        // Flip the sprite if needed
+        if (!isFacingRight && horizontal > 0f)
         {
-            if (!isFacingRight && horizontal > 0f)
-            {
-                Flip();
-            }
-            else if (isFacingRight && horizontal < 0f)
-            {
-                Flip();
-            }
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
         }
 
-        private void FixedUpdate()
+        // Apply gravity tweaks
+        if (rb.linearVelocity.y < 0)
         {
-            rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
-        }
-
-        public void Jump(InputAction.CallbackContext context)
-        {
-            if (context.performed && IsGrounded())
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
-            }
-
-            if (context.canceled && rb.linearVelocity.y > 0f)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-            }
-        }
-
-        private bool IsGrounded()
-        {
-            return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-        }
-
-        private void Flip()
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-
-        public void Move(InputAction.CallbackContext context)
-        {
-            horizontal = context.ReadValue<Vector2>().x;
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
     }
 
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            jumpHeld = true;
+        }
+
+        if (context.canceled)
+        {
+            jumpHeld = false;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+}
