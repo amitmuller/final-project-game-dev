a7313c40a260b8385efebe656834ac7cbcb08751
Maya Heilbrun <Mayah9a@gmail.com>
2025-05-18 17:34:25 +0300

    feat: enemymovement as scriptables objects - week 6 (#29)

---
+++ Assets/Scripts/Characters/Enemies/EnemyMovementData.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Characters.Enemies
+{
+    public enum MovementType
+    {
+        Static,
+        OneDirection,
+        BackAndForth
+    }
+
+    [CreateAssetMenu(fileName = "NewEnemyMovementData", menuName = "Enemy/Movement Data")]
+    public class EnemyMovementData : ScriptableObject
+    {
+        public MovementType movementType;
+
+        [Header("Direction and Speed")]
+        public Vector2 direction = Vector2.right;
+        public float speed = 2f;
+        public float moveDistance = 3f;
+
+        [Header("Hold Times")]
+        public float holdTimeAtStart = 0.5f;
+        public float holdTimeAtEnd = 0.5f;
+
+        [Header("Start Settings")]
+        public bool startMovingRight = true;
+    }
+
+}
