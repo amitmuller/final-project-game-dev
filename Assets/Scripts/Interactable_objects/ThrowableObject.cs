9f85effb72397fd86157464b47cfa095570424b8
mayaheilbrun1 <maya.heilbrun@mail.huji.ac.il>
2025-05-24 14:12:49 +0300

    added the throwing object

---
+++ Assets/Scripts/Interactable_objects/ThrowableObject.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ThrowableObject : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Color originalColor;
+    public Color highlightColor = Color.yellow;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
+    }
+
+    public void Highlight(bool enable)
+    {
+        sr.color = enable ? highlightColor : originalColor;
+    }
+
+    public void fall()
+    {
+        NoiseManager.RaiseNoise(transform.position);
+        Debug.Log($"[PushableObject] NoiseRaised at {transform.position}");
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // You can filter with collision.gameObject.tag if needed
+        fall();
+    }
+}
+
