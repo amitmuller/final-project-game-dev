45ac90ba0d07cc2220d315aecffb573f64a5e6a8
Maya Heilbrun <Mayah9a@gmail.com>
2025-05-24 19:35:16 +0300

    feat: icons appear when needed - week 7

--- Assets/Scripts/Interactable_objects/ThrowableObject.cs
+++ Assets/Scripts/Interactable_objects/ThrowableObject.cs
@@ -6,17 +6,41 @@ public class ThrowableObject : MonoBehaviour
     private SpriteRenderer sr;
     private Color originalColor;
     public Color highlightColor = Color.yellow;
+    private GameObject indicatorInstance;
 
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         originalColor = sr.color;
+        var prefab = Resources.Load<GameObject>("takeThrowIcon");
+        if (prefab)
+        {
+            indicatorInstance = Instantiate(prefab, transform);
+            indicatorInstance.transform.localPosition = new Vector3(0, 1f, 0); // adjust offset as needed
+            indicatorInstance.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Couldn't load hideIcon prefab from Resources", this);
+        }
+        
     }
 
     public void Highlight(bool enable)
     {
         sr.color = enable ? highlightColor : originalColor;
+        if (indicatorInstance != null)
+        {
+            indicatorInstance.SetActive(enable);
+        }
     }
+    
+    public void grab()
+    {
+        indicatorInstance.SetActive(false);
+    }
+    
+    
 
     public void fall()
     {
