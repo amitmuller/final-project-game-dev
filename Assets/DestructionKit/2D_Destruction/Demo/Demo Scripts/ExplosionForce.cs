1c58b1ba36a4d5743234357f0987d09aac4eff2c
Maya Heilbrun <Mayah9a@gmail.com>
2025-06-08 23:38:08 +0300

    feat: Break level - week 10  (#73)
    * feat: break level - week 10
    
    * merge conflicts
    
    * fix merge

--- Assets/DestructionKit/2D_Destruction/Demo/Demo Scripts/ExplosionForce.cs
+++ Assets/DestructionKit/2D_Destruction/Demo/Demo Scripts/ExplosionForce.cs
@@ -13,6 +13,7 @@ public class ExplosionForce : MonoBehaviour {
     /// </summary>
     /// <param name="position">location of the explosion</param>
 	public void doExplosion(Vector3 position){
+		Debug.Log($"Explosion triggered at position: {position}");
 		transform.localPosition = position;
 		StartCoroutine(waitAndExplode());
 	}
@@ -25,10 +26,11 @@ public class ExplosionForce : MonoBehaviour {
 		yield return new WaitForFixedUpdate();
 		
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position,radius);
+		Debug.Log($"Found {colliders.Length} colliders in explosion radius");
      
 		foreach(Collider2D coll in colliders){
-			// Debug.Log(coll.gameObject.name);
 			if(coll.GetComponent<Rigidbody2D>()&&coll.gameObject.name!="Player"){
+				Debug.Log($"Applying explosion force to: {coll.gameObject.name}");
                 AddExplosionForce(coll.GetComponent<Rigidbody2D>(), force, transform.position, radius, upliftModifer);
 			}
 		}
@@ -48,15 +50,16 @@ public class ExplosionForce : MonoBehaviour {
 		float wearoff = 1 - (dir.magnitude / explosionRadius);
         Vector3 baseForce = dir.normalized * explosionForce * wearoff;
         baseForce.z = 0;
+		Debug.Log($"Adding force {baseForce} to {body.gameObject.name}");
 		body.AddForce(baseForce);
 
-        if (upliftModifer != 0)
+        if (upliftModifier != 0)
         {
             float upliftWearoff = 1 - upliftModifier / explosionRadius;
             Vector3 upliftForce = Vector2.up * explosionForce * upliftWearoff;
             upliftForce.z = 0;
             body.AddForce(upliftForce);
+			Debug.Log($"Adding uplift force {upliftForce} to {body.gameObject.name}");
         }
-		
 	}
 }
