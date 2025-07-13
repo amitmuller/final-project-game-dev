d7f1c4c79fdcf4613cdc049361366f170d1cda6a
mayaheilb <Mayah9a@gmail.com>
2025-07-13 11:13:44 +0300

    feat: opening scene - week 14

--- Assets/Scripts/UI/startScene.cs
+++ Assets/Scripts/UI/startScene.cs
@@ -1,45 +1,81 @@
-using System;
-using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
+using UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-namespace UI
-{
-    public class startScene:MonoBehaviour
+
+
+    public class StartSceneController : MonoBehaviour
     {
+        [Header("Frame Sequence")]
+        [Tooltip("List of frames to show sequentially with durations.")]
+        [SerializeField] private SequenceFrame openingFrame;
+        [SerializeField] private List<SequenceFrame> frames = new List<SequenceFrame>();
         
-        [SerializeField] private GameObject startCaption;
-        [SerializeField] private MMF_Player feedback;
-        private Animator _animator;
-        private bool afterAnimation = false; 
+    
+        private bool afterAnimation = false;
+        private bool sequenceStarted = false;
 
         private void Awake()
         {
-            startCaption.SetActive(false);
-            _animator = GetComponent<Animator>();
-        }
-
-        public void onEndStartAnimation()
-        {
-            startCaption.SetActive(true);
-            afterAnimation = true;
+            // Ensure all frames are hidden initially
+            foreach (var frame in frames)
+            {
+                if (frame.frameObject != null)
+                    frame.frameObject.SetActive(false);
+            }
+            openingFrame.frameObject.SetActive(true);
         }
         
+
+        /// <summary>
+        /// Bound to your "Play" input action in the Input System.
+        /// </summary>
         public void OnPressPlay(InputAction.CallbackContext context)
         {
-            if (afterAnimation)
+            print("OnPressPlay");
+            print(!context.performed);
+            print(sequenceStarted);
+            
+            if (!context.performed || sequenceStarted)
+                return;
+            openingFrame.frameObject.SetActive(false);
+
+            StartCoroutine(PlaySequence());
+        }
+
+        /// <summary>
+        /// Plays frames one after another, each for its specified duration.
+        /// </summary>
+        private IEnumerator PlaySequence()
+        {
+            int totalFrames = frames.Count;
+            for (int i = 1; i < totalFrames; i++)
             {
-                startCaption.SetActive(false);
-                _animator.SetTrigger("start");
+                frames[i].frameObject.SetActive(true);
+                yield return new WaitForSecondsRealtime(frames[i].displayTime);
+                if (i == totalFrames-1)
+                {
+                    OnEndScene();
+                }
+                frames[i].frameObject.SetActive(false);
+                
             }
         }
         
+        void Update() {
+            if (Input.GetKeyDown(KeyCode.S))
+                OnEndScene();
+        }
+
+        /// <summary>
+        /// Loads the game scene (index 1).
+        /// </summary>
         public void OnEndScene()
         {
             SceneManager.LoadScene(1);
         }
     }
-    
-    
-}
+
