f049579475b352ba300d8e8a64209e32f27bba5c
mayaheilb <Mayah9a@gmail.com>
2025-07-16 16:13:22 +0300

    fix: open scene + sounds

--- Assets/Scripts/UI/startSceneEvents.cs
+++ Assets/Scripts/UI/startSceneEvents.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using Spine;
+using Spine.Unity;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -13,6 +15,10 @@ public class StartSceneEvents : MonoBehaviour
     [SerializeField] private AudioSource trainSource;
     [SerializeField] private AudioSource DinoSource;
     
+    [Tooltip("One‐shot train sound effect")]
+    [Header("Spine Clips")] public SkeletonAnimation skeletonAnimationGoon;
+    
+    
     
     public void OnTrain()
     {
@@ -22,7 +28,28 @@ public class StartSceneEvents : MonoBehaviour
             trainSource.Play();
         }
     }
-    
+    private void OnEnable()
+    {
+        if (skeletonAnimationGoon != null)
+            skeletonAnimationGoon.AnimationState.Event += HandleSpineEvent;
+    }
+
+    private void OnDisable()
+    {
+        if (skeletonAnimationGoon != null)
+            skeletonAnimationGoon.AnimationState.Event -= HandleSpineEvent;
+    }
+
+    private void HandleSpineEvent(TrackEntry trackEntry, Spine.Event e)
+    {
+        print("event ");
+        switch (e.Data.Name)
+        {
+            case "voice":
+                OnSound();
+                break;
+        }
+    }
     public void OnSound()
     {
         if (DinoSource != null)
@@ -31,16 +58,21 @@ public class StartSceneEvents : MonoBehaviour
             DinoSource.Play();
         }
     }
+    
+    public void OnGoonFinished()
+    {
+        var entry = skeletonAnimationGoon.state.SetAnimation(2, "idleSmoking", false);
+        entry.Complete += e =>
+        {
+            skeletonAnimationGoon.state.SetEmptyAnimation(1, 0.1f);
+            cameraFade.FadeOutOverTime(false, ()=>SceneManager.LoadScene(1));
+        };
+    }
+
 
     public void OnEndAnimation()
     {
         cameraFade.FadeOutOverTime(false, ()=>SceneManager.LoadScene(1));
     }
-
-    void Update()
-    {
-        // cheat: skip to end
-        if (Keyboard.current.sKey.wasPressedThisFrame)
-            SceneManager.LoadScene(1);
-    }
+    
 }
