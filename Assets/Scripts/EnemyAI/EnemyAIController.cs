d239deb03f0e980067f8ec5d2b24af4688ffea76
Maya Heilbrun <Mayah9a@gmail.com>
2025-07-16 17:44:23 +0300

    feat: collider on hands (#174)

--- Assets/Scripts/EnemyAI/EnemyAIController.cs
+++ Assets/Scripts/EnemyAI/EnemyAIController.cs
@@ -370,17 +370,7 @@ public class EnemyAIController : MonoBehaviour
         // forward the event into whatever state we’re in
         _currentState.OnNoiseRaised(worldPos, this);
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
-        if (collision.CompareTag("Player") && !IsPlayerHiding())
-        {
-            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
-            GameManager.Instance.checkpoint(collision.transform);
-        }
-    }
+    
 
     private void initIcons()
     {
