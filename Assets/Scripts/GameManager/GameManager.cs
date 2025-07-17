6ba28c9f638d5b46f578c65a06cf77ba6e07a7ad
Nimrod Mallis <nimrodmallis@gmail.com>
2025-07-17 04:35:48 +0300

    Changed time scaling method to fix a bug

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -221,7 +221,7 @@ public class GameManager : MonoBehaviour
 
         inPause = true;
         PauseMenu.SetActive(true);
-        MMTimeScaleEvent.Trigger(MMTimeScaleMethods.For, 0f, 0f, true, 50f, true);
+        Time.timeScale = 0f;
         openSceneCoroutine = StartCoroutine(goToOpenScene());
     }
     public void exitPause()
@@ -235,7 +235,7 @@ public class GameManager : MonoBehaviour
         {
             StopCoroutine(openSceneCoroutine);
         }
-        MMTimeScaleEvent.Unfreeze();
+        Time.timeScale = 1f;
     }
 
     private IEnumerator goToOpenScene()
